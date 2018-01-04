12632b73c557b1c7412035ea554610a750e9528b
René Anderes <randeres@yahoo.de>
2018-01-04 07:29:42 +0100

    Spring-Releases angepasst

--- spring-jpa-rest/src/main/java/org/anderes/edu/jpa/domain/RecipeRepository.java
+++ spring-jpa-rest/src/main/java/org/anderes/edu/jpa/domain/RecipeRepository.java
@@ -3,9 +3,11 @@ package org.anderes.edu.jpa.domain;
 import java.util.Collection;
 import java.util.List;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RecipeRepository extends PagingAndSortingRepository<Recipe, String> {
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, String> {
 
     Collection<Recipe> findByTitleLike(String string);
     
