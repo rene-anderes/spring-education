729f6856ed16ae492ad6e3ef5e26aa05b0b75edb
René Anderes <randeres@yahoo.de>
2018-04-19 09:10:51 +0200

    Spring Config angepasst

--- spring-jpa-rest/src/test/java/org/anderes/spring/configuration/WebMvcConfig.java
+++ spring-jpa-rest/src/test/java/org/anderes/spring/configuration/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package org.anderes.spring.configuration;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.anderes.cookbook.domain.RecipeRepository;
@@ -14,15 +13,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
@@ -59,25 +55,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
         resolvers.add(new PageableHandlerMethodArgumentResolver());
     }
 
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        registry.viewResolver(viewResolver);
+    }
+
     @Bean
     public RecipeRepository getRecipeRepository() {
         return new RecipeRepositoryStub();
-    }
-    
-    @Bean
-    public ViewResolver viewResolver() {
-        final InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/jsp/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
-        
-    @Bean
-    public RequestMappingHandlerAdapter getRequestMappingHandlerAdapter() {
-        final RequestMappingHandlerAdapter handler = new RequestMappingHandlerAdapter();
-        final List<HttpMessageConverter<?>> messageConverters = new ArrayList<>(1);
-        messageConverters.add(new MappingJackson2HttpMessageConverter());
-        handler.setMessageConverters(messageConverters );
-        return handler;
     }
 }
